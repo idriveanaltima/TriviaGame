a9f20d63415a9ef82f9c4f0922980400ba9630ee
JB <jmb2883@gmail.com>
2018-04-12 18:27:57 -0400

    adding more pieces

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,106 @@
 $(document).ready(function () {
-  var number = 4;
+  var number = 0;
   var x;
   var count = 0;
   var userGuess;
   var correct = 0;
   var incorrect = 0;
+  var userValue;
 
   var trivia = {
-    questions: [{question: "what is happening?", a: "t", b: "a", c:"f", d:"d", answer:"d"},{question: "what is cool?", a: "something", b: "nothing", c:"everything", d:"unsure", answer:"c"}],
-    
+    questions: [{
+      question: "what is happening?",
+      a: "t",
+      b: "a",
+      c: "f",
+      d: "d",
+      answer: "d",
+      image: "<img src='assets/images/crystal1.gif'</>"
+    }, {
+      question: "what is cool?",
+      a: "something",
+      b: "nothing",
+      c: "everything",
+      d: "unsure",
+      answer: "c",
+      image: ""
+    }],
+
     start: function () {
+      this.displayQuestion();
+    },
+    setTimer: function () {
+      number = 4
+      $("#time").html("<p> Time Remaining: <span id='timer'></span></p>");
       clearInterval(x);
       x = setInterval(trivia.countdown, 1000);
-      $("#time").html("<p> Time Remaining: <span id='timer'></span></p>");
-      trivia.displayQuestion();
     },
-    countdown: function () { 
+    countdown: function () {
+     
       number--;
       $("#timer").html(number);
-      if (number === 0) {
+      if (number == 0) {
         trivia.stop();
+        trivia.displayAnswer();
       }
     },
     stop: function () {
       clearInterval(x);
-      console.log("game over");
     },
     displayQuestion: function () {
-      $(".main").html("<h2 id='question'>" + trivia.questions[count].question + "</h2>"+"<p class='answer'>"+trivia.questions[count].a+"<p class='answer'>"+trivia.questions[count].b+"<p class='answer'>"+trivia.questions[count].c+"<p class='answer'>"+trivia.questions[count].d+"</p>");
-      count++;
-    },
-   displayConfirm: function () {
-    $(".main").html("<p class='answer'>Correct Answer "+correct+"<p class='answer'>Incorrect Answer"+incorrect+"<p class='answer'>")
-    $(".main").append("<button id='reset'>Start Over</button>")
-    },
-   reset: function () {
-       count = 0;
-       correct = 0;
-       incorrect = 0;
-       number = 4;
-       this.start ();
-   }
+      if (count == trivia.questions.length) {
+        trivia.displayConfirm();
 
- }
+      } else {
+      trivia.setTimer();
+      $(".main").html("<h2 id='question'>" + trivia.questions[count].question + "</h2>" + "<p class='answer' data-value='a'>" + trivia.questions[count].a + "<p class='answer' data-value='b'>" + trivia.questions[count].b + "<p class='answer' data-value='c'>" + trivia.questions[count].c + "<p class='answer' data-value='d'>" + trivia.questions[count].d + "</p>"); 
+      }
+    },
+    displayAnswer: function () {
+      if (userValue == trivia.questions[count].answer) {
+        $(".main").html("Correct Answer")
+        correct++;
+      } else if (userValue !== trivia.questions[count].answer || number === 0){
+        $(".main").html("Not the Correct Answer")  
+        incorrect++;
+      }
+      $(".main").append("<p>"+this.questions[count].image+"</p>")  
+      count++;
+      setTimeout(trivia.displayQuestion, 1000 * 3);
+      
+    },
+    displayConfirm: function () {
+      trivia.stop();
+      $(".main").html("<p class='answer'>Correct Answer " + correct + "<p class='answer'>Incorrect Answer" + incorrect + "<p class='answer'>")
+      $(".main").append("<button id='reset'>Start Over</button>")
+    },
+    
+    resetGame: function () {
+      count = 0;
+      correct = 0;
+      incorrect = 0;
+      number = 4;
+      trivia.start();
+    }
+
+  }
 
   //starts the game
 
   $("#gameStart").on("click", function () {
-    trivia.start(); 
-    
-
+    trivia.start();
   });
 
   $(".main").on("click", ".answer", function (event) {
- console.log(event.data)
- console.log(event.key)
- console.log(event.result)
- console.log(event.value)
- console.log(event.which + "which")
- console.log(event.target + "target")
- console.log(event.metaKey + "meta key")
- console.log(event.currentTarget === this)
- 
- if (count == trivia.questions.length) {
-          trivia.displayConfirm();
-          trivia.stop();
-    
-        } else if (count < trivia.questions.length) {
-            if (trivia.questions.indexOf(userGuess)  == trivia.questions[count].answer)  {
-                $(".main").html("Correct Answer")
-                 correct++;
-                } else {
-                $(".main").html("Not the Correct Answer")
-                incorrect++;
-                }
-              } else {
-          trivia.displayQuestion ();
-      }
-    });
-
-    console.log()
+    userValue = $(this).attr("data-value")
+    if (count < trivia.questions.length) {
+      trivia.stop()
+      trivia.displayAnswer();
+    } 
+  });
 
   $("body").on("click", "#reset", function () {
-        trivia.reset();
+    trivia.resetGame();
   });
-   
-  });
+
+});
